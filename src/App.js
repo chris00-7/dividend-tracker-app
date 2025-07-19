b34fccda898ca587ab4caf10c033b8684ed1e9c5
chris00-7 <147645034+chris00-7@users.noreply.github.com>
2025-07-19 11:34:21 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Dividend Tracker Dashboard</h1>
+      {/* Dashboard and other components will go here */}
+    </div>
+  );
+}
+
+export default App;
+Create App.js as the main component
