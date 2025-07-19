1cf5a01862b15d56c807150f00ffe240a3e3043d
chris00-7 <147645034+chris00-7@users.noreply.github.com>
2025-07-19 11:55:54 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-
 import React from 'react';
 import Dashboard from './components/Dashboard';
+import Simulator from './components/Simulator';
 
 function App() {
   return (
     <div>
       <h1>Dividend Tracker Dashboard</h1>
       <Dashboard />
+      <Simulator />
     </div>
   );
 }
 
 export default App;
-Link Dashboard component to App.js
+Render Simulator component in App.js
+
