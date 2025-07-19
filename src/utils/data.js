2687d0bbbad76cc5747d0803522f038cf818d3ae
chris00-7 <147645034+chris00-7@users.noreply.github.com>
2025-07-19 12:59:34 -0500

    Create data.js

---
+++ src/utils/data.js
@@ -0,0 +1,7 @@
+export const etfData = [
+  { name: 'JEPI', yield: 7.45, monthlyIncome: 45 },
+  { name: 'SCHD', yield: 3.7, monthlyIncome: 22 },
+  { name: 'VYM', yield: 3.2, monthlyIncome: 18 },
+  { name: 'QYLD', yield: 12.1, monthlyIncome: 60 },
+];
+Add data.js with ETF dividend info
