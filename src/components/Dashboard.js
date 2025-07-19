e5fca4e0fb8b58896ac1de150aba444dfd622904
chris00-7 <147645034+chris00-7@users.noreply.github.com>
2025-07-19 11:41:37 -0500

    Create Dashboard.js

---
+++ src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Monthly Dividend Overview</h2>
+      <p>This is where your dividend data will be shown.</p>
+    </div>
+  );
+}
+
+export default Dashboard;
+Create Dashboard component
