bc0d8826668e904c43cb5ef1e1c7da604decaa7d
chris00-7 <147645034+chris00-7@users.noreply.github.com>
2025-08-11 17:14:29 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import Header from './components/Header';
+import ETFSnapshot from './components/ETFSnapshot';
+import DividendList from './components/DividendList';
+import ReinvestmentSimulator from './components/ReinvestmentSimulator';
+import './styles/App.css';
+
+export default function App() {
+  return (
+    <div className="app">
+      <Header title="Dividend Tracker" />
+      <ETFSnapshot />
+      <DividendList />
+      <ReinvestmentSimulator />
+    </div>
+  );
+}
 
